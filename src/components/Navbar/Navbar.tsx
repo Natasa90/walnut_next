5679cd14e36de3f2def4a741085829af4f5fa32f
Natasa90 <natasa.golubovic90@gmail.com>
2025-05-17 19:33:03 -0500

    added translation for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,23 +5,24 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import Image from "next/image";
 import { playfair } from "@/lib/fonts";
 import { LanguageSwitcher } from "../LanguageSwitcher";
-
-const links = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About Us" },
-    { href: "/gallery", label: "Gallery" },
-    { href: "/booking", label: "Booking" },
-    { href: "/contact", label: "Contact" },
-];
+import { useTranslation } from "react-i18next";
 
 export const Navbar = () => {
     const router = useRouter();
+    const { t } = useTranslation("common");
     const [menuOpen, setMenuOpen] = useState(false);
+
+    const links = [
+        { href: "/", label: t("nav.home") },
+        { href: "/about", label: t("nav.about") },
+        { href: "/gallery", label: t("nav.gallery") },
+        { href: "/booking", label: t("nav.booking") },
+        { href: "/contact", label: t("nav.contact") },
+    ];
 
     return (
         <nav className="shadow-md">
             <div className="mx-4 md:mx-10 px-4 py-3 flex justify-between items-center relative">
-                {/* Logo */}
                 <Link href="/">
                     <div className="flex items-center">
                         <Image
@@ -31,7 +32,6 @@ export const Navbar = () => {
                             height={80}
                             className="rounded-full transition-transform duration-300 hover:scale-110 hover:rotate-6 sm:w-[100px] sm:h-[100px]"
                         />
-                        {/* Title hidden on small screens */}
                         <p
                             className={`${playfair.className} ml-4 mb-1 text-2xl hidden lg:block`}
                         >
@@ -39,8 +39,6 @@ export const Navbar = () => {
                         </p>
                     </div>
                 </Link>
-
-                {/* Desktop links */}
                 <div className="hidden md:flex gap-6 mt-2">
                     {links.map((link) => (
                         <Link
@@ -59,8 +57,6 @@ export const Navbar = () => {
                     ))}
                     <LanguageSwitcher />
                 </div>
-
-                {/* Hamburger menu toggle */}
                 <button
                     onClick={() => setMenuOpen(!menuOpen)}
                     className="md:hidden text-2xl absolute top-10 right-4"
@@ -69,18 +65,15 @@ export const Navbar = () => {
                     {menuOpen ? <FaTimes size={36} /> : <FaBars size={36} />}
                 </button>
             </div>
-
-            {/* Mobile menu */}
             {menuOpen && (
                 <>
-                    {/* Overlay to block the background */}
                     <div
                         className="fixed inset-0 bg-black/40 z-40"
                         onClick={() => setMenuOpen(false)}
                     />
 
                     {/* Mobile menu itself */}
-                    <div className="md:hidden fixed top-20 right-4 w-48 bg-white/60 backdrop-blur-lg rounded-lg shadow-lg p-4 z-50 animate-slide-in opacity-0">
+                    <div className="md:hidden fixed top-20 right-4 w-48 bg-white/80 backdrop-blur-lg rounded-lg shadow-lg p-4 z-50 animate-slide-in opacity-0">
                         <div className="flex flex-col items-start gap-4">
                             {links.map((link) => (
                                 <Link
